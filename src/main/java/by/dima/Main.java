f8c0a6229eb12c4ba0af71dba29040eaeef5e2ad
dmitrijmartynov <dotaxai@gmail.com>
2025-04-08 19:43:53 +0300

    logging fix

--- src/main/java/by/dima/Main.java
+++ src/main/java/by/dima/Main.java
@@ -7,32 +7,42 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 public class Main {
     private static final Logger logger = Logger.getLogger("Main.class");
     private static final ConsoleHandler consoleHandler = new ConsoleHandler();
+    private static FileHandler fileHandler;
 
     public static void main(String[] args) {
-        consoleHandler.setLevel(Level.FINE);
-        logger.setUseParentHandlers(false);
-        logger.addHandler(consoleHandler);
-        logger.setLevel(Level.FINE);
 
+        try {
+            fileHandler = new FileHandler("app.log");
+        } catch (IOException e) {
+            System.err.println("Работа вашей программы будет без отслеживания ошибок!");
+        }
+        fileHandler.setLevel(Level.FINE);
+        fileHandler.setFormatter(new SimpleFormatter());
+
+        consoleHandler.setLevel(Level.FINE);
+        consoleHandler.setFilter(r -> (r.getLevel() == Level.INFO || r.getLevel() == Level.FINE));
+        consoleHandler.setFormatter(new SimpleFormatter());
+
+        logger.setUseParentHandlers(false);
+        logger.addHandler(fileHandler);
+        logger.addHandler(consoleHandler);
+
+        logger.setLevel(Level.FINE);
 
         boolean isRunning = true;
         DatagramPacket packet;
         int port = 80;
         byte[] byteBuffer = new byte[1024];
 
+        logger.log(Level.INFO, "Ожидание запросов от клиентов!");
         try (DatagramSocket socket = new DatagramSocket(port)) {
             while (isRunning) {
                 packet = new DatagramPacket(byteBuffer, byteBuffer.length);
-                logger.log(Level.INFO, "Слушаю клиентов...");
                 socket.receive(packet);
                 try (ByteArrayInputStream bis = new ByteArrayInputStream(byteBuffer, 0, packet.getLength());
                      ObjectInputStream ois = new ObjectInputStream(bis)) {
@@ -46,6 +56,9 @@ public class Main {
             }
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Порт уже занят!");
+        } finally {
+            consoleHandler.close();
+            fileHandler.close();
         }
     }
 }
